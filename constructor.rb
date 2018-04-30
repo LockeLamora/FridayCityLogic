61eda257813e9b1331b843538f9fa5902d467dbb
Locke Lamora <alick_gardiner@hotmail.com>
2018-04-30 09:47:55 +0100

    Adding price changes after Baku

--- constructor.rb
+++ constructor.rb
@@ -15,23 +15,23 @@ class Constructor
       @name = 'Mercedes'
       @points = 215
     when 2
-      @cost = 27.5
+      @cost = 28
       @name = 'Ferrari'
       @points = 207
     when 3
-      @cost = 20
+      @cost = 19
       @name = 'RedBull'
       @points = 139
     when 4
-      @cost = 10.5
+      @cost = 11
       @name = 'Force India'
       @points = 57
     when 5
-      @cost = 9
+      @cost = 8.7
       @name = 'Williams'
       @points = 40
     when 6
-      @cost = 9.7
+      @cost = 9.9
       @name = 'Renault'
       @points = 94
     when 7
@@ -43,11 +43,11 @@ class Constructor
       @name = 'Haas'
       @points = 51
     when 9
-      @cost = 9.5
+      @cost = 9.7
       @name = 'McLaren'
       @points = 130
     when 10
-      @cost = 5
+      @cost = 5.7
       @name = 'Sauber'
       @points = 79
     end
--- driver.rb
+++ driver.rb
@@ -13,12 +13,12 @@ class Driver
     costthreshold = 19
     case @number
     when 1
-      @cost = 11.5
+      @cost = 11.8
       @name = 'Alonso'
       @points = 76
       history = [28, 24, 24, 24]
     when 2
-      @cost = 25.3
+      @cost = 24.4
       @name = 'Bottas'
       @points = 93
       history = [19, 37, 37, 7]
@@ -28,72 +28,72 @@ class Driver
       @points = 20
       history = [-12, 19, 13, 16]
     when 4
-      @cost = 7.5
+      @cost = 7.2
       @name = 'Gasly'
       @points = 18
       history = [-14, 28, 4, 14]
     when 5
-      @cost = 6.5
+      @cost = 6.3
       @name = 'Grosjean'
       @points = -3
       history = [-8, 8, -3, -20]
     when 6
-      @cost = 29.8
+      @cost = 30.1
       @name = 'Hamilton'
       @points = 94
       history = [35, 36, 23, 45]
     when 7
-      @cost = 6.5
+      @cost = 6.6
       @name = 'Hartley'
       @points = -4
       history = [9, -2, -11, 18]
     when 8
-      @cost = 12
+      @cost = 11.5
       @name = 'Hulkenberg'
       @points = 63
       history = [18, 22, 23, -8]
     when 9
-      @cost = 4.5
+      @cost = 5.5
       @name = 'Leclerc'
       @points = 31
       history = [15, 12, 4, 30]
     when 10
-      @cost = 7.5
+      @cost = 7.4
       @name = 'Magnussen'
       @points = 29
       history = [-5, 25, 9, 12]
     when 11
-      @cost = 11.3
+      @cost = 10.7
       @name = 'Ocon'
       @points = 23
       history = [7, 8, 8, -6]
     when 12
-      @cost = 12
+      @cost = 12.6
       @name = 'Perez'
       @points = 10
       history = [10, -1, 1, 35]
     when 13
-      @cost = 20
+      @cost = 20.4
       @name = 'Raikonnen'
       @points = 54
       history = [28, -3, 29, 38]
     when 14
-      @cost = 20.5
+      @cost = 19.9
       @name = 'Ricciardo'
       @points = 76
       history = [33, -4, 47, -3]
     when 15
-      @cost = 9.5
+      @cost = 10.3
       @name = 'Sainz'
       @points = 20
       history = [9, 3, 8, 26]
     when 16
-      @cost = 7.5
+      @cost = 7.0
       @name = 'Sirotkin'
       @points = 2
       history = [-14, 10, 6, -13]
     when 17
-      @cost = 8.5
+      @cost = 8.7
       @name = 'Stroll'
       @points = 35
       history = [7, 15, 13, 16]
@@ -103,12 +103,12 @@ class Driver
       @points = 31
       history = [9, 17, 5, 18]
     when 19
-      @cost = 19.7
+      @cost = 18.9
       @name = 'Verstappen'
       @points = 26
       history = [17, -13, 22, -6]
     when 20
-      @cost = 28
+      @cost = 27.8
       @name = 'Vettel'
       @points = 98
       history = [44, 44, 10, 22]
