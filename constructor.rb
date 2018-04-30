59c8d0cad7898752758f9941ea03c8736f2d3f10
Locke Lamora <alick_gardiner@hotmail.com>
2018-04-30 09:43:58 +0100

    Adding latest stats after Baku

--- constructor.rb
+++ constructor.rb
@@ -13,43 +13,43 @@ class Constructor
     when 1
       @cost = 30
       @name = 'Mercedes'
-      @points = 173
+      @points = 215
     when 2
       @cost = 27.5
       @name = 'Ferrari'
-      @points = 152
+      @points = 207
     when 3
       @cost = 20
       @name = 'RedBull'
-      @points = 120
+      @points = 139
     when 4
       @cost = 10.5
       @name = 'Force India'
-      @points = 18
+      @points = 57
     when 5
       @cost = 9
       @name = 'Williams'
-      @points = 27
+      @points = 40
     when 6
       @cost = 9.7
       @name = 'Renault'
-      @points = 66
+      @points = 94
     when 7
       @cost = 7.8
       @name = 'Toro Rosso'
-      @points = 14
+      @points = 46
     when 8
       @cost = 5.5
       @name = 'Haas'
-      @points = 44
+      @points = 51
     when 9
       @cost = 9.5
       @name = 'McLaren'
-      @points = 93
+      @points = 130
     when 10
       @cost = 5
       @name = 'Sauber'
-      @points = 38
+      @points = 79
     end
 
     @points /= numraces.to_f
--- driver.rb
+++ driver.rb
@@ -16,102 +16,102 @@ class Driver
       @cost = 11.5
       @name = 'Alonso'
       @points = 76
-      history = [28, 24, 24]
+      history = [28, 24, 24, 24]
     when 2
       @cost = 25.3
       @name = 'Bottas'
       @points = 93
-      history = [19, 37, 37]
+      history = [19, 37, 37, 7]
     when 3
       @cost = 5.5
       @name = 'Ericsson'
       @points = 20
-      history = [-12, 19, 13]
+      history = [-12, 19, 13, 16]
     when 4
       @cost = 7.5
       @name = 'Gasly'
       @points = 18
-      history = [-14, 28, 4]
+      history = [-14, 28, 4, 14]
     when 5
       @cost = 6.5
       @name = 'Grosjean'
       @points = -3
-      history = [-8, 8, -3]
+      history = [-8, 8, -3, -20]
     when 6
       @cost = 29.8
       @name = 'Hamilton'
       @points = 94
-      history = [35, 36, 23]
+      history = [35, 36, 23, 45]
     when 7
       @cost = 6.5
       @name = 'Hartley'
       @points = -4
-      history = [9, -2, -11]
+      history = [9, -2, -11, 18]
     when 8
       @cost = 12
       @name = 'Hulkenberg'
       @points = 63
-      history = [18, 22, 23]
+      history = [18, 22, 23, -8]
     when 9
       @cost = 4.5
       @name = 'Leclerc'
       @points = 31
-      history = [15, 12, 4]
+      history = [15, 12, 4, 30]
     when 10
       @cost = 7.5
       @name = 'Magnussen'
       @points = 29
-      history = [-5, 25, 9]
+      history = [-5, 25, 9, 12]
     when 11
       @cost = 11.3
       @name = 'Ocon'
       @points = 23
-      history = [7, 8, 8]
+      history = [7, 8, 8, -6]
     when 12
       @cost = 12
       @name = 'Perez'
       @points = 10
-      history = [10, -1, 1]
+      history = [10, -1, 1, 35]
     when 13
       @cost = 20
       @name = 'Raikonnen'
       @points = 54
-      history = [28, -3, 29]
+      history = [28, -3, 29, 38]
     when 14
       @cost = 20.5
       @name = 'Ricciardo'
       @points = 76
-      history = [33, -4, 47]
+      history = [33, -4, 47, -3]
     when 15
       @cost = 9.5
       @name = 'Sainz'
       @points = 20
-      history = [9, 3, 8]
+      history = [9, 3, 8, 26]
     when 16
       @cost = 7.5
       @name = 'Sirotkin'
       @points = 2
-      history = [-14, 10, 6]
+      history = [-14, 10, 6, -13]
     when 17
       @cost = 8.5
       @name = 'Stroll'
       @points = 35
-      history = [7, 15, 13]
+      history = [7, 15, 13, 16]
     when 18
       @cost = 8
       @name = 'Vandoorne'
       @points = 31
-      history = [9, 17, 5]
+      history = [9, 17, 5, 18]
     when 19
       @cost = 19.7
       @name = 'Verstappen'
       @points = 26
-      history = [17, -13, 22]
+      history = [17, -13, 22, -6]
     when 20
       @cost = 28
       @name = 'Vettel'
       @points = 98
-      history = [44, 44, 10]
+      history = [44, 44, 10, 22]
     end
 
     if @cost <= costthreshold
