e6fab65b77bb946c2b7c72879cc5f50562f7f054
Locke Lamora <alick_gardiner@hotmail.com>
2018-05-14 15:37:02 +0100

    neatening up the output with some dodgy tables

--- lib/output.rb
+++ lib/output.rb
@@ -1,49 +1,47 @@
 def print_teams(teams)
   print_header 'NEW TEAMS FROM SCRATCH:'
+  print_table_header
   teams = teams[0..50]
 
   teams.each do |team|
     if team.active ==false
       next
     end
-    output = ""
+    drivers = ""
     for driver in team.drivers
-      output+= "#{driver.name}"
+      drivers+= "#{driver.name}"
       if driver.name == team.turbo.name
-        output+= "(T)"
+        drivers+= "(T)"
       end
-      output += ","
+      drivers += ","
     end
-
-    output += "   | Constructor: #{team.constructor.name}"
-    output += "   | average points of this team so far: #{team.points.round(2)}"
-    output += "   | cost: #{team.cost}"
-
-    puts output
+    printrow(drivers, team.constructor.name, team.cost, team.points.round(2))
   end
+
+  print_table_footer
 end
 
 def print_interrace_teams(teams)
   print_header('TRADES THIS RACE:')
+  print_table_header
   teams.each do |team|
     if team.active == false
       next
     end
-    output = ""
+    drivers = ""
     for driver in team.drivers
-      output+= "#{driver.name}"
+      drivers+= "#{driver.name}"
       if driver.name == team.turbo.name
-        output+= "(T)"
+        drivers+= "(T)"
       end
-      output += ","
+      drivers += ","
     end
 
-    output += "   | Constructor: #{team.constructor.name}"
-    output += "   | average points of this team so far: #{team.points.round(2)}"
-    output += "   | cost: #{team.cost}"
+    printrow(drivers, team.constructor.name,team.cost,team.points.round(2) )
 
-    puts output
   end
+
+  print_table_footer
 end
 
 def print_team_errors(errors)
@@ -60,4 +58,42 @@ def print_header(text)
   puts '=========================='
   puts text
   puts '=========================='
+end
+
+def print_table_header(type=:header)
+  drivercolumn = ' '*21 + 'DRIVERS' + ' '*21
+  vdivider = '|'
+  constructorscolumn = ' '*5 + 'CONSTRUCTOR' + ' '*5
+  averagepointsheader = ' '* 5 + 'Avg Pts' + ' '* 5
+  costheader = ' COST '
+  header = vdivider + drivercolumn + vdivider + constructorscolumn + vdivider\
+  + averagepointsheader + vdivider + costheader + vdivider
+
+  hdivider = '_' * header.length
+
+  if type == :header
+    puts hdivider
+    puts header
+    puts hdivider
+  else
+    puts hdivider
+  end
+
+end
+
+def print_table_footer
+  print_table_header(:footer)
+end
+
+def printrow(drivers, constructor, cost, averagepointsperrace)
+  vdivider = '|'
+  drivers = drivers + (' ' * (49 - drivers.length))
+  constructor = constructor + (' ' * (21 - constructor.length))
+  cost = cost.to_s + (' ' * (6-cost.to_s.length))
+  averagepointsperrace = averagepointsperrace.to_s + (' ' * (17-averagepointsperrace.to_s.length))
+
+  row = vdivider + drivers + vdivider + constructor + vdivider + averagepointsperrace + vdivider\
+ + cost + vdivider
+
+  puts row
 end
